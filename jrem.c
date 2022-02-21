02ee510232d3fe1caeb973ba54d74cd07a454da5
5841320 <5841320@git.sd5.gpaas.net>
2022-02-21 16:30:33 +0100

    add remove and show cmd

--- jrem.c
+++ jrem.c
@@ -1,5 +1,5 @@
 #define _GNU_SOURCE
-#include "stdio.h"
+#include <stdio.h>
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,7 +7,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+/**
+ * @brief add_cmd
+ * @param fd
+ * @param arg
+ */
 void add_cmd(int fd,char* arg)
 {
 	int arg_size = strlen(arg);
@@ -15,19 +19,54 @@ void add_cmd(int fd,char* arg)
 	write(fd,"\n",1);
 	close(fd);
 }
+/**
+ * @brief rm_cmd
+ * @param index
+ */
 void rm_cmd(int index)
 {
-	FILE *f_stream= fdopen(".jrem","w+");
-	
+	char temp[60];
+	int line =1;
+	if(index!=0)
+	{
+		FILE *f_stream= fopen(".jrem","r");
+		FILE *f_stream_dup = fopen(".jrem-dup","w");
+		while(fgets(temp,60,f_stream)!=NULL)
+		{
+			if(line!=index)
+			{
+				fputs(temp,f_stream_dup);
+
+			}
+			++line;
+
+		}
+		fclose(f_stream);
+		fclose(f_stream_dup);
+		remove(".jrem");
+		rename(".jrem-dup",".jrem");
+
+	}else
+	{
+		printf("can't remove command without index\n");
+	}
+
+
 }
+/**
+ * @brief show_cmd
+ * @param index
+ */
 void show_cmd(int index)
 {
 	FILE *f_stream= fopen(".jrem","r");
-	if(f_stream==NULL){perror("fdopen()"); exit(1);}	
+	if(f_stream==NULL){perror("fopen()"); exit(1);}
 	int line =1;
 	char str[60];
+	bool empty=true;
 	while(fgets(str,60,f_stream)!=NULL)
 	{
+		empty=false;
 		if(index==0)
 		{
 			printf("[%d] ",line);
@@ -48,12 +87,27 @@ void show_cmd(int index)
 
 		}
 	}
-	
+	if(empty)printf("No command register\n");
+
 	fclose(f_stream);
 }
+/**
+ * @brief exec_cmd
+ * @param fd
+ * @param index
+ */
 void exec_cmd(int fd, int index){}
+/**
+ * @brief find
+ * @param fd
+ */
 void find(int fd,char *){}
-
+/**
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char *argv[])
 {
 	int fd;
@@ -73,22 +127,28 @@ int main(int argc, char *argv[])
 	switch(cmd[0])
 	{
 		case 'a':
-		{add_cmd(fd,arg);}
+		add_cmd(fd,arg);
+		break;
 		case 'r':
 		{
-			close(fd); 
-			rm_cmd(atoi(arg))
+			close(fd);
+			int index = arg==NULL ? 0 : atoi(arg);
+			rm_cmd(index);
 		}
+		break;
 		case 's':
 		{
-			close(fd);
-			show_cmd(atoi(arg));	
+			int index = arg==NULL ? 0 : atoi(arg);
+			show_cmd(index);
 		}
+		break;
 		case 'e':
 		break;
 		case 'i':
 		break;
 		case 'f':
+		break;
+		case 'h':
 		break;
 		default:
 		printf("Unknow command\n");
