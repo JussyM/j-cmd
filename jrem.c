b175dbea8eb940ac5acd19b673412b63f9d9937e
JussyM <51095966+JussyM@users.noreply.github.com>
2022-02-23 15:54:06 +0100

    refector

--- jrem.c
+++ jrem.c
@@ -7,6 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+char* file_name()
+{
+
+    char * home_dir = getenv("HOME");
+    int fd;
+    if((home_dir=getenv("HOME"))!=NULL)
+    {
+        char file_name[]="/.jrem";
+        strcat(home_dir,file_name);
+    }
+    return home_dir;
+}
+
 /**
  * @brief add_cmd
  * @param fd
@@ -14,65 +27,55 @@
  */
 void add_cmd(char* arg)
 {
-	char * home_dir = file_name();
-	fd= open(home_dir,O_WRONLY|O_CREAT|O_APPEND,0666);
-	if(fd<0)
-	{
-		printf("can not open config file\n");
-		exit(1);
-	}else
-	{
-	   int arg_size = strlen(arg);
-	   write(fd,arg,arg_size);
-	   write(fd,"\n",1);
-	   close(fd);
+    char * home_dir = file_name();
+    fd= open(home_dir,O_WRONLY|O_CREAT|O_APPEND,0666);
+    if(fd<0)
+    {
+        printf("can not open config file\n");
+        exit(1);
+    }else
+    {
+        int arg_size = strlen(arg);
+        write(fd,arg,arg_size);
+        write(fd,"\n",1);
+        close(fd);
 
-	}
+    }
 }
 
-char* file_name()
-{
 
-        char * home_dir = getenv("HOME");
-        int fd;
-        if((home_dir=getenv("HOME"))!=NULL)
-        {
-        char file_name[]="/.jrem";
-        strcat(home_dir,file_name);
-	}
-return home_dir;
-}
 /**
  * @brief rm_cmd
  * @param index
  */
 void rm_cmd(int index)
 {
-	char temp[60];
-	int line =1;
-	if(index!=0)
-	{
-		FILE *f_stream= fopen(".jrem","r");
-		FILE *f_stream_dup = fopen(".jrem-dup","w");
-		while(fgets(temp,60,f_stream)!=NULL)
-		{
-			if(line!=index)
-			{
-				fputs(temp,f_stream_dup);
+    char * home_dir = file_name();
+    char temp[60];
+    int line =1;
+    if(index!=0)
+    {
+        FILE *f_stream= fopen(home_dir,"r");
+        FILE *f_stream_dup = fopen(".jrem-dup","w");
+        while(fgets(temp,60,f_stream)!=NULL)
+        {
+            if(line!=index)
+            {
+                fputs(temp,f_stream_dup);
 
-			}
-			++line;
+            }
+            ++line;
 
-		}
-		fclose(f_stream);
-		fclose(f_stream_dup);
-		remove(".jrem");
-		rename(".jrem-dup",".jrem");
+        }
+        fclose(f_stream);
+        fclose(f_stream_dup);
+        remove(".jrem");
+        rename(".jrem-dup",".jrem");
 
-	}else
-	{
-		printf("can't remove command without index\n");
-	}
+    }else
+    {
+        printf("can't remove command without index\n");
+    }
 
 
 }
@@ -82,37 +85,38 @@ void rm_cmd(int index)
  */
 void show_cmd(int index)
 {
-	FILE *f_stream= fopen(".jrem","r");
-	if(f_stream==NULL){perror("fopen()"); exit(1);}
-	int line =1;
-	char str[60];
-	bool empty=true;
-	while(fgets(str,60,f_stream)!=NULL)
-	{
-		empty=false;
-		if(index==0)
-		{
-			printf("[%d] ",line);
-			puts(str);
-			++line;
-		}else
-		{
-			if(line==index)
-			{
-				printf("[%d] ",line);
-				puts(str);
-				break;
-			}
-			else
-			{
-				++line;
-			}
+    char * home_dir = file_name();
+    FILE *f_stream= fopen(home_dir,"r");
+    if(f_stream==NULL){perror("fopen()"); exit(1);}
+    int line =1;
+    char str[60];
+    bool empty=true;
+    while(fgets(str,60,f_stream)!=NULL)
+    {
+        empty=false;
+        if(index==0)
+        {
+            printf("[%d] ",line);
+            puts(str);
+            ++line;
+        }else
+        {
+            if(line==index)
+            {
+                printf("[%d] ",line);
+                puts(str);
+                break;
+            }
+            else
+            {
+                ++line;
+            }
 
-		}
-	}
-	if(empty)printf("No command register\n");
+        }
+    }
+    if(empty)printf("No command register\n");
 
-	fclose(f_stream);
+    fclose(f_stream);
 }
 /**
  * @brief exec_cmd
@@ -129,16 +133,17 @@ void exec_cmd(int fd, int index)
  */
 int find(char *arg)
 {
-	FILE *f_stream= fopen(".jrem","r");
-	char temp[60];
-	int line=1;
-	while(fgets(temp,60,f_stream)!=NULL)
-	{
-		if(strcmp(arg,temp)==0) return line;
-		++line;
-	}
-	fclose(f_stream);
-	return line;
+    char * home_dir = file_name();
+    FILE *f_stream= fopen(home_dir,"r");
+    char temp[60];
+    int line=1;
+    while(fgets(temp,60,f_stream)!=NULL)
+    {
+        if(strcmp(arg,temp)==0) return line;
+        ++line;
+    }
+    fclose(f_stream);
+    return line;
 
 }
 /**
@@ -149,48 +154,48 @@ int find(char *arg)
  */
 int main(int argc, char *argv[])
 {
-	if(argc>3)
-	{
-		printf("invalid input\n");
-		exit(1);
-	}
-	char* cmd = argv[1];
-	char* arg = argv[2];
-	switch(cmd[0])
-	{
-		case 'a':
-		add_cmd(arg);
-		break;
-		case 'r':
-		{
-			int index = arg==NULL ? 0 : atoi(arg);
-			rm_cmd(index);
-		}
-		break;
-		case 's':
-		{
-			int index = arg==NULL ? 0 : atoi(arg);
-			show_cmd(index);
-		}
-		break;
-		case 'e':
-		break;
-		case 'i':
-		break;
-		case 'f':
-		{
-			int result =find(arg);
-			if(result!=0) printf("The command is at line: %d\n",result);
-			else printf("No command found\n");
-		}
-		break;
-		case 'h':
-		break;
-		default:
-		printf("Unknow command\n");
-		break;
+    if(argc>3)
+    {
+        printf("invalid input\n");
+        exit(1);
+    }
+    char* cmd = argv[1];
+    char* arg = argv[2];
+    switch(cmd[0])
+    {
+    case 'a':
+        add_cmd(arg);
+        break;
+    case 'r':
+    {
+        int index = arg==NULL ? 0 : atoi(arg);
+        rm_cmd(index);
+    }
+        break;
+    case 's':
+    {
+        int index = arg==NULL ? 0 : atoi(arg);
+        show_cmd(index);
+    }
+        break;
+    case 'e':
+        break;
+    case 'i':
+        break;
+    case 'f':
+    {
+        int result =find(arg);
+        if(result!=0) printf("The command is at line: %d\n",result);
+        else printf("No command found\n");
+    }
+        break;
+    case 'h':
+        break;
+    default:
+        printf("Unknow command\n");
+        break;
 
-	}
+    }
 
-	return 0;
+    return 0;
 }
