66087a603aa36b4c1005730bf9cfdc1d74e59a3f
JussyM <jussymidedji@gmail.com>
2022-09-07 00:38:45 +0200

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "-----Welcome to jrem cmd installation-----\n"
-echo -e "==>Verifying make and gcc\n"
+echo -e "--Verifying make and gcc\n"
 if ! command -v gcc &> /dev/null 
 then
 	echo "gcc could not be found please install gcc and retry\n"
@@ -14,9 +14,15 @@ else
 	echo -e "make and gcc present on your system\n"
 	if make
 	then
-		echo -e "compilation done succes...\nMoving binary file to $HOME/.local/bin/\n"
-		mv jrem $HOME/.local/bin
+		dst_path=$HOME/.local/bin/
+		echo -e "compilation done succes...\nMoving binary file to ${dst_path}\n"
+		if [ ! -d "$dst_path" ]; then
+			echo -e "path ${dst_path} null creating one"
+			mkdir "$dst_path"
+		fi
+		mv jrem $HOME/.local/bin/
 		echo "Done you can now use jrem\n"
+		echo "Do not forget to add ${dst_path} to your class path"
 	else
 		echo -e "Compilation error operation aborted\n"
 	fi
